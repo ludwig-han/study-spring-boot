6f3129de2613433afa0ffae12ea42de20c47cd69
Ludwig Han <ludwighan.dev@gmail.com>
2026-09-23 14:26:01 +0900

    test: add user controller tests

--- src/test/java/com/example/study_spring_boot/controller/UserControllerTest.java
+++ src/test/java/com/example/study_spring_boot/controller/UserControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.study_spring_boot.controller;
 
+import com.example.study_spring_boot.controller.dto.PostResponse;
 import com.example.study_spring_boot.controller.dto.UserResponse;
+import com.example.study_spring_boot.domain.User;
 import com.example.study_spring_boot.repository.PostRepository;
 import com.example.study_spring_boot.service.UserService;
 import org.junit.jupiter.api.Test;
@@ -35,18 +37,33 @@ class UserControllerTest {
     PostRepository postRepository;
 
 
-//    @Test
-//    void getUsers_success() throws Exception {
-//        List<UserResponse> userResponses = new ArrayList<>();
-//
-//
-//        mockMvc.perform(
-//                get("/users")
-//        )
-//                .andExpect(status().isOk());
-//
-//        verify(userService).getUsers();     // 이것도 해야하나?
-//    }
+    @Test
+    void getUsers_success() throws Exception {
+        // given
+        List<UserResponse> userResponses = new ArrayList<>();
+        UserResponse userResponse1 = new UserResponse(1L, "user1", "user@email1");
+        UserResponse userResponse2 = new UserResponse(2L, "user2", "user@email2");
+
+        userResponses.add(userResponse1);
+        userResponses.add(userResponse2);
+
+        when(userService.getUsers()).thenReturn(userResponses);
+
+        // when
+        mockMvc.perform(
+                get("/users")
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].id").value(1L))
+                .andExpect(jsonPath("$[0].name").value("user1"))
+                .andExpect(jsonPath("$[0].email").value("user@email1"))
+                .andExpect(jsonPath("$[1].id").value(2L))
+                .andExpect(jsonPath("$[1].name").value("user2"))
+                .andExpect(jsonPath("$[1].email").value("user@email2"));
+
+        verify(userService).getUsers();     // 이것도 해야하나?
+    }
 
     @Test
     void getUser_success() throws Exception {
@@ -75,10 +92,34 @@ class UserControllerTest {
                 .andExpect(status().isNotFound());
     }
 
-//    @Test
-//    void getPostsByUserId() throws Exception {
-//
-//    }
+    @Test
+    void getPostsByUserId_success() throws Exception {
+        // given
+        List<PostResponse> postResponses = List.of(
+                new PostResponse(2L, 11L, "title1", "content1"),
+                new PostResponse(2L, 12L, "title2", "content2")
+        );
+
+        when(userService.getPostsByUserId(2L))
+                .thenReturn(postResponses);
+
+        // when + then
+        mockMvc.perform(
+                get("/users/2/posts")
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].userId").value(2L))
+                .andExpect(jsonPath("$[0].id").value(11L))
+                .andExpect(jsonPath("$[0].title").value("title1"))
+                .andExpect(jsonPath("$[0].content").value("content1"))
+                .andExpect(jsonPath("$[1].userId").value(2L))
+                .andExpect(jsonPath("$[1].id").value(12L))
+                .andExpect(jsonPath("$[1].title").value("title2"))
+                .andExpect(jsonPath("$[1].content").value("content2"));
+
+        verify(userService).getPostsByUserId(2L);
+    }
 
     @Test
     void createUser_success() throws Exception {
